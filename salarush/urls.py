3ef32625f79d579c2abf8c3da854e02cac4df14b
sahadporn charnlertlakha <59825486+Sahadporn@users.noreply.github.com>
2020-09-26 17:20:09 +0700

    fuck you sesay

--- salarush/urls.py
+++ salarush/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/', views.AddView.as_view(), name='table'),
     path('add_item/', views.add_item, name='add_item'),
-    path('subtract_item/', views.subtract_item, name='subtract_item')
+    # path('subtract_item/', views.subtract_item, name='subtract_item')
 ]
--- salarush/views.py
+++ salarush/views.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from django.urls import reverse
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 
 from .models import RushBook, RushListModel
 
@@ -14,17 +15,22 @@ def index(request):
 def add_item(request):
     n = request.POST['list']
     m = request.POST['amount']
+    t = request.POST['action']
     book = RushBook.objects.get(id=1)
-    book.add_list(m, n, True)
+    if t == "income":
+        book.add_list(m, n, True)
+    else:
+        book.add_list(m, n, False)
+    messages.info(request, f"total {book.total_rush()}")
     return HttpResponseRedirect(reverse('salarush:table', args=(1,)))
 
 
-def subtract_item(request):
-    book = get_object_or_404(RushBook, pk=1)
-    n = request.POST['list']
-    m = request.POST['amount']
-    book.add_list(m, n, False)
-    return HttpResponseRedirect(reverse('salarush:table', args=(1,)))
+# def subtract_item(request):
+#     book = get_object_or_404(RushBook, pk=1)
+#     n = request.POST['list']
+#     m = request.POST['amount']
+#     book.add_list(m, n, False)
+#     return HttpResponseRedirect(reverse('salarush:table', args=(1,)))
 
 
 class AddView(generic.ListView):
@@ -32,4 +38,9 @@ class AddView(generic.ListView):
     context_object_name = 'rush_list'
 
     def get_queryset(self):
-        return RushListModel.objects.filter(post_date__lte=timezone.now()).order_by('-post_date')
+        return RushListModel.objects.filter(post_date__lte=timezone.now()).order_by('post_date')
+
+    # def get_context_data(self):
+    #     context = RushBook.objects.get(pk=1)
+    #     return {'user': context, }
+
